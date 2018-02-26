7c2da9d2d273f19a88fb81b4d4ebbccd34150c7a
Talya Shraga <talyashra@gmail.com>
2018-02-26 16:43:25 +0200

    Setting UCCA-OLD

--- Server/ucca/settings-chelem.py
+++ Server/ucca/settings-chelem.py
@@ -1,7 +1,7 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ucca-prod2',# 'ucca'
+        'NAME': 'ucca-old',
         'OPTIONS': {
             'options': '-c search_path=ucca'
         },
